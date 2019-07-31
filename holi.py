4066dc35df11b920d370ddcefeee568ec9ea0701
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-07-31 15:47:54 +0530

    Add files via upload

---
+++ holi.py
@@ -0,0 +1,5 @@
+def holi():
+    d=input()
+    a={'Monday':'no','Tuesday':'no','Wednesday':'no','Thursday':'no','Friday':'no','Saturday':'yes','Sunday':'yes','monday':'no','tuesday':'no','wednesday':'no','thursday':'no','friday':'no','saturday':'yes','sunday':'yes'}
+    print(a[d])
+holi()
